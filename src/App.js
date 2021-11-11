253ba94cf6e78a0453244f25bcf76f53ac2f0e24
kkristy24 <karmova.kristina@gmail.com>
2021-11-11 21:09:22 +0300

    lesson 11

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return (
+        <div className="app-wrapper">
+            <header className="header">
+                <img src='https://picfiles.alphacoders.com/216/thumb-1920-216599.png'/>
+            </header>
+            <nav className="nav">
+                <div>
+                    <div>
+                        <a>Profile</a>
+                    </div>
+                    <div>
+                        <a>Messages</a>
+                    </div>
+                    <div>
+                        <a>News</a>
+                    </div>
+                    <div>
+                        <a>Music</a>
+                    </div>
+                    <div>
+                        <a>Settings</a>
+                    </div>
+                </div>
+            </nav>
+            <div className="content">
+                <div className="mainImg">
+                    <img src='https://zabor.bz/zakaz/img/market/fotooboi_voda_img_5.jpg'/>
+                </div>
+                <div>
+                    <img src='https://i.pinimg.com/originals/fa/11/0f/fa110feb86350d897056f54e18c83567.png'/>
+                    ava + description
+                </div>
+                <div>
+                    My posts
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
