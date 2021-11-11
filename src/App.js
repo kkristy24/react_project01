60e9f02d880f9437da72b9e69935160b6f13fe5e
kkristy24 <karmova.kristina@gmail.com>
2021-11-11 22:22:23 +0300

    fix grid and image

--- src/App.js
+++ src/App.js
@@ -29,12 +29,23 @@ const App = () => {
                 <div className="mainImg">
                     <img src='https://zabor.bz/zakaz/img/market/fotooboi_voda_img_5.jpg'/>
                 </div>
-                <div>
+                <div className="avaImg">
                     <img src='https://i.pinimg.com/originals/fa/11/0f/fa110feb86350d897056f54e18c83567.png'/>
                     ava + description
                 </div>
                 <div>
                     My posts
+                    <div>
+                        New post
+                    </div>
+                    <div>
+                        <div>
+                            Post 1
+                        </div>
+                        <div>
+                            Post 2
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
