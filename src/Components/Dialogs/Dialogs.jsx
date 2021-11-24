943459b7b89e1c9ad3364f2a966b25f31101b187
kkristy24 <karmova.kristina@gmail.com>
2021-11-24 21:08:36 +0300

    layout dialogs

--- src/Components/Dialogs/Dialogs.jsx
+++ src/Components/Dialogs/Dialogs.jsx
@@ -1,9 +1,31 @@
 import React from "react";
+import s from './Dialogs.module.css';
 
 const Dialogs = () => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog + ' ' + s.active}>
+                    Mia
+                </div>
+                <div className={s.dialog}>
+                    Alex
+                </div>
+                <div className={s.dialog}>
+                    Cristy
+                </div>
+                <div className={s.dialog}>
+                    Kate
+                </div>
+                <div className={s.dialog}>
+                    Constantin
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Yo</div>
+                <div className={s.message}>Bla bla</div>
+                <div className={s.message}>How are you?</div>
+            </div>
         </div>
     );
 }
